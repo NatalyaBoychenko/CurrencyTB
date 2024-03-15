50165293b4a3c4eb56eaf1df16a27c413e655b5d
DmytroKaplun <newtonknight1990@gmail.com>
2024-03-15 13:20:36 +0200

    add telegram

---
+++ src/main/java/com/telegrambot/features/telegram/BotConstants.java
@@ -0,0 +1,9 @@
+package com.telegrambot.features.telegram;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class BotConstants {
+    public static final String BOT_NAME = "name";
+    public static final String BOT_TOKEN = "token";
+}
---
+++ src/main/java/com/telegrambot/features/telegram/CurrencyTelegramBot.java
@@ -0,0 +1,34 @@
+package com.telegrambot.features.telegram;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class CurrencyTelegramBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BotConstants.BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BotConstants.BOT_TOKEN;
+    }
+
+}
