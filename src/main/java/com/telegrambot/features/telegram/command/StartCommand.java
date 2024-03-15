3c48aa1e20fe4a4c91a0bfbe9dfcef2f782a01df
DmytroKaplun <newtonknight1990@gmail.com>
2024-03-15 17:17:26 +0200

    added UTF_8

--- src/main/java/com/telegrambot/features/telegram/command/StartCommand.java
+++ src/main/java/com/telegrambot/features/telegram/command/StartCommand.java
@@ -5,7 +5,6 @@ import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
