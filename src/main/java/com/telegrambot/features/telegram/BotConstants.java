460efa62e53180f9d947fcc010da1427e93947c5
Natalia Boichenko <123556403+NatalyaBoychenko@users.noreply.github.com>
2024-03-19 12:26:48 +0200

    Update BotConstants.java

--- src/main/java/com/telegrambot/features/telegram/BotConstants.java
+++ src/main/java/com/telegrambot/features/telegram/BotConstants.java
@@ -9,5 +9,5 @@ public class BotConstants {
     public static final String BOT_NAME = "name";
     public static final String BOT_TOKEN = "token";
     public static final String  INFO = new String("Отримати інфо".getBytes(), StandardCharsets.UTF_8);
-    public static final String SETTINGS = new String("Налаштування".getBytes(), StandardCharsets.UTF_8);
+    public static final String SETTINGS = new String("Налаштування".getBytes(), StandardCharsets.UTF_8); 
 }
