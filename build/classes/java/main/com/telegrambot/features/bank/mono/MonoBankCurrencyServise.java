da19fbc9a00c1e5a7acb86239b24f4e8b1ca2cab
Natalia Boichenko <123556403+NatalyaBoychenko@users.noreply.github.com>
2024-03-22 10:21:48 +0200

    Update MonoBankCurrencyServise.java
    add line

--- build/classes/java/main/com/telegrambot/features/bank/mono/MonoBankCurrencyServise.java
+++ build/classes/java/main/com/telegrambot/features/bank/mono/MonoBankCurrencyServise.java
@@ -16,21 +16,98 @@ import java.util.Arrays;
 import java.util.List;
 //edit
 public class MonoBankCurrencyServise implements CurrencyServise {
-    public MonoBankCurrencyServise() {
+
+     @Override
+    public double getBuyRate(CurrencyList currency) {
+
+        List<CurrencyMonoItem> currencyItems = getRate();
+        float converted = 0;
+
+        if (currency.name().equalsIgnoreCase("usd")) {
+            converted = currencyItems.stream()
+                    .filter(it -> it.getCurrencyCodeA() == 840)
+                    .filter(it -> it.getCurrencyCodeB() == 980)
+                    .map(CurrencyMonoItem::getRateBuy)
+                    .findFirst()
+                    .orElseThrow();
+        } else  if (currency.name().equalsIgnoreCase("eur")){
+            converted = currencyItems.stream()
+                    .filter(it -> it.getCurrencyCodeA() == 978)
+                    .filter(it -> it.getCurrencyCodeB() == 980)
+                    .map(CurrencyMonoItem::getRateBuy)
+                    .findFirst()
+                    .orElseThrow();
+        }
+
+        return converted;
     }
 
-    public double getRate(Currency currency) throws IOException, InterruptedException {
-        Gson gson = (new GsonBuilder()).setPrettyPrinting().create();
-        HttpClient client = HttpClient.newHttpClient();
-        String url = "https://api.monobank.ua/bank/currency";
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        JsonMB[] todosArray = (JsonMB[])gson.fromJson((String)response.body(), JsonMB[].class);
-        List<JsonMB> curencyList = new ArrayList(Arrays.asList(todosArray));
-        return (double)(Float)curencyList.stream().filter((x) -> {
-            return x.getCcy() == currency;
-        }).map((x) -> {
-            return x.getBuy();
-        }).findFirst().orElseThrow();
+    @Override
+    public double getSellRate(CurrencyList currency) {
+        List<CurrencyMonoItem> currencyItems = getRate();
+
+        float converted = 0;
+
+        if (currency.name().equalsIgnoreCase("usd")) {
+            converted = currencyItems.stream()
+                    .filter(it -> it.getCurrencyCodeA() == 840)
+                    .filter(it -> it.getCurrencyCodeB() == 980)
+                    .map(CurrencyMonoItem::getRateSell)
+                    .findFirst()
+                    .orElseThrow();
+        } else  if (currency.name().equalsIgnoreCase("eur")){
+            converted = currencyItems.stream()
+                    .filter(it -> it.getCurrencyCodeA() == 978)
+                    .filter(it -> it.getCurrencyCodeB() == 980)
+                    .map(CurrencyMonoItem::getRateSell)
+                    .findFirst()
+                    .orElseThrow();
+        }
+
+        return converted;
     }
+
+
+    private List<CurrencyMonoItem> getRate() {
+
+        String json;
+        try {
+            json = Jsoup
+                    .connect(MONO_URL)
+                    .ignoreContentType(true)
+                    .get()
+                    .body()
+                    .text();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalStateException("Cannot connect to Monobank API");
+        }
+
+        Type typeTocken = TypeToken
+                .getParameterized(List.class, CurrencyMonoItem.class)
+                .getType();
+        List<CurrencyMonoItem> currencyItems = new Gson().fromJson(json, typeTocken);
+
+        return currencyItems;
+    }
+
+
+    
+    // public MonoBankCurrencyServise() {
+    // }
+
+    // public double getRate(Currency currency) throws IOException, InterruptedException {
+    //     Gson gson = (new GsonBuilder()).setPrettyPrinting().create();
+    //     HttpClient client = HttpClient.newHttpClient();
+    //     String url = "https://api.monobank.ua/bank/currency";
+    //     HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+    //     HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+    //     JsonMB[] todosArray = (JsonMB[])gson.fromJson((String)response.body(), JsonMB[].class);
+    //     List<JsonMB> curencyList = new ArrayList(Arrays.asList(todosArray));
+    //     return (double)(Float)curencyList.stream().filter((x) -> {
+    //         return x.getCcy() == currency;
+    //     }).map((x) -> {
+    //         return x.getBuy();
+    //     }).findFirst().orElseThrow();
+    // }
 }
