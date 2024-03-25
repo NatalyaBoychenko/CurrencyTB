795bb8e0f26f22c77cc1bd2f57f1398db9a97653
Natalya Boichenko <natalya.andriychuk@gmail.com>
2024-03-25 18:01:22 +0200

    add reminder to main

--- src/main/java/com/telegrambot/features/telegram/CurrencyTelegramBot.java
+++ src/main/java/com/telegrambot/features/telegram/CurrencyTelegramBot.java
@@ -1,6 +1,5 @@
 package com.telegrambot.features.telegram;
 
-import com.telegrambot.features.settings.Settings;
 import com.telegrambot.features.settings.ExchangeMessage;
 import com.telegrambot.features.settings.Settings;
 import com.telegrambot.features.settings.StorageInMemoryRepo;
@@ -8,16 +7,6 @@ import com.telegrambot.features.telegram.command.*;
 import com.telegrambot.features.telegram.util.BotConstants;
 import com.telegrambot.features.telegram.util.Keyboard;
 import com.telegrambot.features.telegram.util.TGPictures;
-import com.sun.tools.javac.util.Names;
-import com.telegrambot.features.settings.Settings;
-import com.telegrambot.features.settings.ExchangeMessage;
-import com.telegrambot.features.settings.StorageInMemoryRepo;
-import com.telegrambot.features.telegram.command.BankSetting;
-import com.telegrambot.features.telegram.command.CurrencySetting;
-import com.telegrambot.features.telegram.command.Reminder;
-import com.telegrambot.features.telegram.command.RoundRate;
-import com.telegrambot.features.telegram.util.BotConstants;
-import com.telegrambot.features.telegram.util.Keyboard;
 import lombok.SneakyThrows;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -25,15 +14,10 @@ import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Optional;
-import org.telegram.telegrambots.meta.api.objects.MessageEntity;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
 import java.time.LocalTime;
 import java.util.Date;
 import java.util.HashMap;
@@ -43,22 +27,20 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import static com.telegrambot.features.currency.dto.Currency.EUR;
-import static com.telegrambot.features.currency.dto.Currency.USD;
-import static com.telegrambot.features.telegram.util.BotConstants.*;
+import static com.telegrambot.features.telegram.util.BotConstants.BOT_NAME;
+import static com.telegrambot.features.telegram.util.BotConstants.BOT_TOKEN;
 
 public class CurrencyTelegramBot extends TelegramLongPollingBot {
     private final RoundRate roundRate;
     private final BankSetting bankSetting;
     private final Reminder reminder;
     private final CurrencySetting currency;
-    StorageInMemoryRepo storageInMemory;
+    private StorageInMemoryRepo storageInMemory;
     private Language language;
 
-    //======================================================
-    Map<Long, String> users = new HashMap<>();
-    boolean flag1 = true;
-    //==============================================
+    private Map<Long, String> users = new HashMap<>();
+    private boolean flag1 = true;
+
 
     @Override
     public String getBotUsername() {
@@ -77,17 +59,11 @@ public class CurrencyTelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        //====================================================================
-        Long chatId = update.getCallbackQuery().getMessage().getChatId();
-        if (BotConstants.TIME_REMINDER1.contains(update.getCallbackQuery().getData())) {
-            users.put(chatId, update.getCallbackQuery().getData());
-            System.out.println("users = " + users);
-        }
-//===========================================================================================
-        methodReminderTime();
 
 
-        //===================================================================
+
+
+
         SendMessage sendMessage = new SendMessage();
 
         if (update.hasCallbackQuery()) {
@@ -98,12 +74,20 @@ public class CurrencyTelegramBot extends TelegramLongPollingBot {
                     storageInMemory.getSettingForConcreteUser(chatId) : Settings.getDefaultSettings(chatId, storageInMemory);
 
             handleCallback(update.getCallbackQuery(), settings,storageInMemory, sendMessage);
+
+            if (BotConstants.TIME_REMINDER1.contains(update.getCallbackQuery().getData())) {
+                users.put(chatId, update.getCallbackQuery().getData());
+                System.out.println("users = " + users);
+            }
+
+            methodReminderTime(settings);
+
         } else if (update.hasMessage()) {
             handleMessage(update.getMessage());
         }
     }
 
-    private void methodReminderTime() {
+    private void methodReminderTime(Settings settings) {
         if (flag1) {
             ScheduledExecutorService executorServiceGeneral = Executors.newSingleThreadScheduledExecutor();
 
@@ -112,42 +96,33 @@ public class CurrencyTelegramBot extends TelegramLongPollingBot {
                         long minutesSerGen = LocalTime.now().getMinute();
                         long secondsSerGen = LocalTime.now().getSecond();
 
-                        System.out.println("General.scheduleAtFixedRate(() -> { Start" + hourSerGen +":"
-                                + minutesSerGen +":" + secondsSerGen);
-                        System.out.println("users = " + users);
-
-                        System.out.println("BotConstans.TIME_REMINDER2.contains(hourSerGen) = " + BotConstants.TIME_REMINDER2.contains(hourSerGen));
-
                         if (BotConstants.TIME_REMINDER2.contains(hourSerGen)) {
 
-                            System.out.println("BotConstans.TIME_MINUTE.contains(minutesSerGen) = " + BotConstants.TIME_MINUTE.contains(minutesSerGen));
                             if (BotConstants.TIME_MINUTE.contains(minutesSerGen)) {
 
                                 for (Long key : users.keySet()) {
                                     long timeReminder = Long.parseLong(users.get(key));
                                     long start = 0;
 
-                                    System.out.println("(timeReminder != 0) && (timeReminder == hourSerGen + 1) = " + ((timeReminder != 0) && (timeReminder == hourSerGen + 1)));
                                     if ((timeReminder != 0) && (timeReminder == hourSerGen + 1)) {
 
                                         start = (59 - minutesSerGen) * 60 + 60 - secondsSerGen;
-                                        ;
+
                                         System.out.println("start = " + start);
                                         if (start != 0) {
 
                                             ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
                                             executorService.schedule(() -> {
-// ******************************** вставити ментод відпраки повідомлення*********************
+
+                                                TGPictures.sendImage(settings.getChatId(), "reminder", this);
                                                 SendMessage answer = new SendMessage();
-                                                answer.setText("TEXT MASSAGE  :" + LocalTime.now().getHour()
-                                                        + ":" + LocalTime.now().getMinute() + ":" + LocalTime.now().getSecond());
+                                                answer.setText(ExchangeMessage.printMessage(settings));
                                                 answer.setChatId(key);
                                                 try {
                                                     execute(answer);
                                                 } catch (TelegramApiException e) {
                                                     throw new RuntimeException(e);
                                                 }
- //***************************************************************************************
                                             }, start, TimeUnit.SECONDS);
                                         }
                                     }
@@ -172,7 +147,6 @@ public class CurrencyTelegramBot extends TelegramLongPollingBot {
                 settings.setLanguage("eng");
                 storageInMemory.addSetting(chatId, settings);
                 sendMessage.setChatId(chatId);
-//            String text = BotConstants.getEng().get("MESSSAGE");
                 sendMessage.setText(BotConstants.getNameButton("eng", "MESSAGE"));
                 sendMessage.setReplyMarkup(Keyboard.setStartKeyboard(settings));
                 execute(sendMessage);
@@ -181,7 +155,6 @@ public class CurrencyTelegramBot extends TelegramLongPollingBot {
                 settings.setLanguage("ukr");
                 storageInMemory.addSetting(chatId, settings);
                 sendMessage.setChatId(chatId);
-//            String text = BotConstants.getUkr().get("MESSSAGE");
                 sendMessage.setText(BotConstants.getNameButton("ukr", "MESSAGE"));
                 sendMessage.setReplyMarkup(Keyboard.setStartKeyboard(settings));
                 execute(sendMessage);
@@ -190,7 +163,6 @@ public class CurrencyTelegramBot extends TelegramLongPollingBot {
                 TGPictures.sendImage(chatId, "info", this);
                 sendMessage.setChatId(chatId);
                 sendMessage.setText(ExchangeMessage.printMessage(settings));
-//                sendMessage.setText(Settings.getDefaultSettings(chatId));
                 sendMessage.setReplyMarkup(Keyboard.setStartKeyboard(settings));
                 execute(sendMessage);
             }
@@ -207,9 +179,7 @@ public class CurrencyTelegramBot extends TelegramLongPollingBot {
                 execute(sendMessage);
             }
             case "ROUNDED_INDEX" -> {
-//                roundRate.handleCallbackRoundRate(callbackQuery, settings, storageInMemory , this);
                 sendMessage.setChatId(chatId);
-//                sendMessage.setText(BotConstants.getNameButton(settings.getLanguage(), ROUNDED_INDEX));
                 sendMessage.setText(BotConstants.getNameButton(settings.getLanguage(), "ROUNDED_INDEX"));
                 sendMessage.setReplyMarkup(Keyboard.setRoundRateKeyboard(settings));
                 execute(sendMessage);
--- src/main/java/com/telegrambot/features/telegram/util/BotConstants.java
+++ src/main/java/com/telegrambot/features/telegram/util/BotConstants.java
@@ -10,8 +10,8 @@ import java.util.ResourceBundle;
 @UtilityClass
 public class BotConstants {
 
-    public static final String BOT_NAME = "";
-    public static final String BOT_TOKEN = "";
+    public static final String BOT_NAME = "NatBoiCurBot";
+    public static final String BOT_TOKEN = "6412532350:AAHp0kMrq78YDNXWT6M2zMOsgceuLDWauk8";
     public static final String PRIVAT_URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
     public static final String MONO_URL = "https://api.monobank.ua/bank/currency";
     public static final String NBU_URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
@@ -20,7 +20,7 @@ public class BotConstants {
     public static final List<Long> TIME_MINUTE = List.of(57L,58L,59L);
 
     public static String getNameButton(String language, String name){
-        Locale locale = new Locale(language);
+        Locale locale = new Locale.Builder().setLanguage(language).build();
         ResourceBundle rb = ResourceBundle.getBundle("names", locale);
         return rb.getString(name);
 
