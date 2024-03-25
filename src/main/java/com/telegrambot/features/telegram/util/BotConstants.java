880a2f75e053e1d811f63f015538953e6eeb4b8a
Natalya Boichenko <natalya.andriychuk@gmail.com>
2024-03-25 18:03:12 +0200

    add reminder to main

--- src/main/java/com/telegrambot/features/telegram/util/BotConstants.java
+++ src/main/java/com/telegrambot/features/telegram/util/BotConstants.java
@@ -17,7 +17,7 @@ public class BotConstants {
     public static final String NBU_URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
     public static final List<String> TIME_REMINDER1 = List.of("0","18","8","9","10","11","12","13","14","15","16","17");
     public static final List<Long> TIME_REMINDER2 = List.of(6L,7L,8L,9L,10L,11L,12L,13L,14L,15L,16L,17L);
-    public static final List<Long> TIME_MINUTE = List.of(57L,58L,59L);
+    public static final List<Long> TIME_MINUTE = List.of(57L,58L,56L);
 
     public static String getNameButton(String language, String name){
         Locale locale = new Locale.Builder().setLanguage(language).build();
